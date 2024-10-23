8e086ccaeee1fb258863ef6d2311b97410dfaa03
MashuduRamoba <119120286+MashuduRamoba@users.noreply.github.com>
2024-10-23 13:56:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+// Navbar
+
+const navbar = document.querySelector("nav");
+window.addEventListener("scroll",() =>navbar.classList.toggle("sticky",window.scrollY>0));
+
+const menu = document.querySelector("menu");
+const toggleMenu =()=> menu.classList.toggle("active");
+
+
+document.querySelector(".menu-btn").addEventListener("click",toggleMenu);
+document.querySelector(".close-btn").addEventListener("click",toggleMenu);
+
+document .querySelectorAll(".menu a").forEach((link)=>link.addEventListener("click",toggleMenu));
+
+
+//swipper 
+const swiper =new Swiper(".swiper",{
+    //parameters
+    slidesPerView:1,
+    direction:"horizontal",
+    lazyLoading :true,
+    centeredSlides:true,
+    loop:true,
+    keyboard:{
+        enabled:true,
+    },
+    autoplay:{delay:3000,},
+
+pagination:{
+    el:".swiper-pagination"
+},
+
+navigation:{
+    nextEl:".swiper-button-next",
+    prevEl:".swiper-button-prev",
+},
+
+});
+
+
+
